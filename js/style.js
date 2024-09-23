fb792abc5a6ebe7ff06e9b47b0363f64a0b44a1c
Fayqe-Montasir <swe.montasir@gmail.com>
2024-09-23 22:19:35 +0600

    home

--- js/style.js
+++ js/style.js
@@ -5,80 +5,59 @@ document.getElementById('blog-btn').addEventListener('click',function(){
 
 document.getElementById('card-btn').addEventListener('click', function () {
     const inputOne = getInputValueById('input-one');
-    // const inputTwo = getInputValueById('input-two');
     const donateBalanceOne = getBalanceValueById('donate-balance-one');
-    // const donateBalanceTwo = getBalanceValueById('donate-balance-two')
     const newBalance = donateBalanceOne + inputOne;
-    // const newBalanceTwo = donateBalanceTwo + inputTwo;
     document.getElementById('donate-balance-one').innerText = newBalance;
-    // document.getElementById('donate-balance-two').innerText = newBalanceTwo;
     const totalBalance = getBalanceValueById('total-balance')
-    // const totalBalanceTwo = getBalanceValueById('total-balance')
     const newTotalBalance = totalBalance - inputOne;
-    // const newTotalBalanceTwo = totalBalanceTwo - inputTwo;
-
+    
     document.getElementById('total-balance').innerText = newTotalBalance;
-    // document.getElementById('total-balance').innerText = newTotalBalanceTwo;
-
-
-   
-    // console.log(inputTwo)
     const historyContainer = document.getElementById('History-title');
     historyContainer.innerHTML = `  
      <div class="border p-8 space-y-4 rounded-2xl mt-6 ">
      <h3 class="text-xl font-bold"> ${inputOne} Taka is Donated for famine-2024 at Feni, Bangladesh</h3>
-    <p>{newDate()}</P>
+    <p> Date: ${new Date().toDateString() } </P>
      </div>
+     ` 
+}); 
+document.getElementById('card-btn-two').addEventListener('click', function (){
+    
+    const inputTwo = getInputValueById('input-two');
+    const donateBalanceTwo = getBalanceValueById('donate-balance-two')
+    const newBalanceTwo = donateBalanceTwo + inputTwo;
+    document.getElementById('donate-balance-two').innerText = newBalanceTwo;
+    const totalBalanceTwo = getBalanceValueById('total-balance')
+    const newTotalBalanceTwo = totalBalanceTwo - inputTwo;
+    document.getElementById('total-balance').innerText = newTotalBalanceTwo;
+    
+    
+    
+    const historyContainer = document.getElementById('History-title');
+    historyContainer.innerHTML = `  
     <div class="border p-8 space-y-4 rounded-2xl mt-6">
-       <h3 class="text-xl font-bold">Taka is Donated for Flood Relief in Feni,Bangladesh</h3>
-     </div>
+    <h3 class="text-xl font-bold">${inputTwo} Taka is Donated for Flood Relief in Feni,Bangladesh</h3>
+    <p> Date: ${new Date().toDateString() } </P>
+    </div>
+    `  
+})
+document.getElementById('card-btn-3').addEventListener('click', function () {
+    const inputThree = getInputValueById('input-3');
+    const donateBalanceThree = getBalanceValueById('donate-balance-three')
+    const newBalanceThree = donateBalanceThree + inputThree;
+    document.getElementById('donate-balance-three').innerText = newBalanceThree;
+    console.log(newBalanceThree)
+    const totalBalanceThree = getBalanceValueById('total-balance')
+    const newTotalBalanceThree = totalBalanceThree - inputThree;
+    document.getElementById('total-balance').innerText = newTotalBalanceThree;
+
+    const historyContainer = document.getElementById('History-title');
+    historyContainer.innerHTML = `  
      <div class=" border p-8 space-y-4 rounded-2xl  mt-6">
-       <h3 class="text-xl font-bold">$ Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h3>
-       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, autem, maiores </p>
+       <h3 class="text-xl font-bold">${inputThree} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h3>
+       <p>  Date: ${new Date().toDateString() } </p>
      </div>
      `  
-// document.getElementById('card-btn').addEventListener('click', function () {
-   
-
-    // const historyContainer = document.getElementById('History-title');
-    // historyContainer.innerHTML = ` 
-    // <div class="border p-8 space-y-4 rounded-2xl mt-6 ">
-    //  <h3 class="text-xl font-bold">0 Taka is Donated for famine-2024 at Feni, Bangladesh</h3>
-    
-    //  </div> 
-    //   <div class="border p-8 space-y-4 rounded-2xl mt-6">
-    //    <h3 class="text-xl font-bold">${inputTwo}Taka is Donated for Flood Relief in Feni,Bangladesh</h3>
-    //  </div>
-    //  <div class=" border p-8 space-y-4 rounded-2xl  mt-6">
-    //    <h3 class="text-xl font-bold">$ Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h3>
-    //    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, autem, maiores </p>
-    //  </div>
-    //  `  
 })
-// document.getElementById('card-btn').addEventListener('click', function (event) {
-//     const inputOne = getInputValueById('input-one');
-//     const donateBalanceOne = getBalanceValueById('donate-balance-one')
-//     const newBalance = donateBalanceOne + inputOne;
-//     document.getElementById('donate-balance-one').innerText = newBalance;
-//     const totalBalance = getBalanceValueById('total-balance')
-//     const newTotalBalance = totalBalance - inputOne;
-//     document.getElementById('total-balance').innerText = newTotalBalance;
-
-//     const historyContainer = document.getElementById('History-title');
-//     historyContainer.innerHTML = `  
-//      <div class="border p-8 space-y-4 rounded-2xl mt-6 ">
-//      <h3 class="text-xl font-bold"> ${inputOne} Taka is Donated for famine-2024 at Feni, Bangladesh</h3>
-    
-//      </div>
-//      <div class="border p-8 space-y-4 rounded-2xl mt-6">
-//        <h3 class="text-xl font-bold">${inputTwo} Taka is Donated for Flood Relief in Feni,Bangladesh</h3>
-//      </div>
-//      <div class=" border p-8 space-y-4 rounded-2xl  mt-6">
-//        <h3 class="text-xl font-bold">$ Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h3>
-//        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, autem, maiores </p>
-//      </div>
-//      `  
-// })
 
 
 
